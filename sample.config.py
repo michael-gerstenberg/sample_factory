b71f973f8111fc53507a9f949d44bce301799102
michael-gerstenberg <michael@gerstenberg.ch>
2022-02-15 12:16:09 +0100

    init

---
+++ sample.config.py
@@ -0,0 +1,9 @@
+# script config
+
+# factory setup
+quantity_molding_machines = 3
+parts = ['windshield','wheel','body','brezel','wing']
+
+# database config
+mongo_connection_string = 'mongodb+srv://xxx:xxx@claschda.da1ep.mongodb.net/test?authSource=admin&replicaSet=atlas-rk5bb1-shard-0&readPreference=primary&appname=MongoDB%20Compass&ssl=true'
+mongo_database = 'sample_manufacturing'
